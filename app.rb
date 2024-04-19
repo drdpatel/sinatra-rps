f5e92fab5a973266e9bbf03b37d8501ec22b6522
drdpatel <165212603+drdpatel@users.noreply.github.com>
2024-04-19 15:13:37 +0000

    rock

--- app.rb
+++ app.rb
@@ -39,3 +39,19 @@ get("/paper") do
 
   erb(:paper)
 end
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "scissors"
+    @outcome = "tied"
+  elsif @comp_move == "rock"
+    @outcome = "lost"
+  else @comp_move == "paper"
+    @outcome = "won"  
+  end
+
+  erb(:scissors)
+end
